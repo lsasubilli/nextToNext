f0c08b2c667643305594531f48aceebe9ef67a8d
Lalith Sasubilli <68266855+lsasubilli@users.noreply.github.com>
2022-02-09 21:49:59 -0800

    Update nextToNext.py

--- nextToNext.py
+++ nextToNext.py
@@ -1,10 +1,12 @@
 def has_int():
+  #initializing an incrementing type variable
   count=0
   random_arr=[1,3,5,5,2]
   for x in range(len(random_arr)):
     if random_arr[count]==random_arr[count+1]:
-      print("The value of "+str(random_arr[count])+" is "+str(random_arr[count]))
+      print(str(random_arr[count])+" is next to "+str(random_arr[count])+" in this array!")
       break;
     count=count+1
     
 has_int()
+#lets go it works
